0afe28cf83d087629438c8307f06433d85ce62dc
Samuel <samlee1097@yahoo.com>
2021-09-20 11:29:17 -0400

    commit tree c73e623bca4e5dce2405ccfd211a3a90d680d5a0

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,8 +1,12 @@
 import React from "react";
 import whoseHouse from "./houses/whoseHouse";
+import { colors, gryffMascot } from "./houses/Gryffindor";
 
 function Hogwarts() {
   whoseHouse(); // => "HAGRID'S HOUSE!"
-
+  console.log(colors);
+  gryffMascot();
   return <h1>Welcome to Hogwarts!</h1>;
 }
+
+export default Hogwarts
